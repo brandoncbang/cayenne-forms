d72aacdfcf8442f1cec765a89ba6f9016315d1aa
Brandon Bang <brandoncharlesbang@gmail.com>
2023-05-25 21:31:05 -0400

    Add empty state

--- resources/js/Pages/Forms/Index.vue
+++ resources/js/Pages/Forms/Index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
 import { Link } from "@inertiajs/vue3";
-import { EllipsisVerticalIcon } from "@heroicons/vue/20/solid";
+import { EllipsisVerticalIcon, PlusIcon } from "@heroicons/vue/20/solid";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import Pagination from "@/Components/Dashboard/Pagination.vue";
 
@@ -14,7 +14,10 @@ const maxUnreadEntriesCountDisplayed = 999_999;
 
 <template>
     <AuthenticatedLayout title="Forms">
-        <div class="overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl">
+        <div
+            v-if="forms.data.length > 0"
+            class="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl"
+        >
             <ul
                 role="list"
                 class="divide-y divide-gray-100"
@@ -95,8 +98,29 @@ const maxUnreadEntriesCountDisplayed = 999_999;
                     </div>
                 </li>
             </ul>
-
             <Pagination :paginator="forms" class="px-4 py-5 border-t border-gray-200 sm:px-6" />
+        </div>
+        <div v-else class="px-4 py-5 text-center sm:px-6">
+            <svg
+                class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                aria-hidden="true"
+            >
+                <path
+                    vector-effect="non-scaling-stroke" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M9 13h6m-3-3v6m-9 1V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+                />
+            </svg>
+            <h3 class="mt-2 text-sm font-semibold text-gray-900">No forms</h3>
+            <p class="mt-1 text-sm text-gray-500">Get started by creating a new form.</p>
+            <div class="mt-6">
+                <Link
+                    :href="route('forms.create')"
+                    class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                    <PlusIcon class="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+                    New Form
+                </Link>
+            </div>
         </div>
     </AuthenticatedLayout>
 </template>
