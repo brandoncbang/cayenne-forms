76d38d3afb572b65d156ab2010d3b4bb8ef29524
Brandon Bang <brandoncharlesbang@gmail.com>
2023-06-09 22:21:57 -0400

    Update no selected Entry display per tab

--- resources/js/Pages/Entries/Index.vue
+++ resources/js/Pages/Entries/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { displayDate, displayDateTime, displayNumber, getFormEmbedCode } from '@/helpers.js';
 import CopyButton from '@/Components/Dashboard/CopyButton.vue';
 import { ArchiveBoxIcon, InboxArrowDownIcon, PencilIcon, TrashIcon } from '@heroicons/vue/20/solid/index.js';
@@ -8,6 +8,9 @@ import { InformationCircleIcon } from '@heroicons/vue/24/outline/index.js';
 import { router, Link } from '@inertiajs/vue3';
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/vue';
 import SimplePagination from '@/Components/Dashboard/SimplePagination.vue';
+import ThinArrowDownIcon from '@/Components/Dashboard/Icons/ThinArrowDownIcon.vue';
+import ThinTrashIcon from '@/Components/Dashboard/Icons/ThinTrashIcon.vue';
+import ThinArchiveBoxIcon from '@/Components/Dashboard/Icons/ThinArchiveBoxIcon.vue';
 
 const props = defineProps({
     form: Object,
@@ -17,26 +20,31 @@ const props = defineProps({
 const loadingSelectedEntry = ref(false);
 const selectedEntry = ref(null);
 
-const tabs = [
-    {
-        name: 'Inbox',
-        href: route('forms.entries.index', { form: props.form }),
-        icon: InboxArrowDownIcon,
-        current: route().current('forms.entries.index', { form: props.form, filter: null }),
-    },
-    {
-        name: 'Archive',
-        href: route('forms.entries.index', { form: props.form, filter: 'archived' }),
-        icon: ArchiveBoxIcon,
-        current: route().current('forms.entries.index', { form: props.form, filter: 'archived' }),
-    },
-    {
-        name: 'Trash',
-        href: route('forms.entries.index', { form: props.form, filter: 'trashed' }),
-        icon: TrashIcon,
-        current: route().current('forms.entries.index', { form: props.form, filter: 'trashed' }),
-    },
-];
+const tabs = computed(() => {
+    return [
+        {
+            name: 'Inbox',
+            href: route('forms.entries.index', { form: props.form }),
+            icon: InboxArrowDownIcon,
+            current: route().current('forms.entries.index', { form: props.form, filter: null }),
+        },
+        {
+            name: 'Archive',
+            href: route('forms.entries.index', { form: props.form, filter: 'archived' }),
+            icon: ArchiveBoxIcon,
+            current: route().current('forms.entries.index', { form: props.form, filter: 'archived' }),
+        },
+        {
+            name: 'Trash',
+            href: route('forms.entries.index', { form: props.form, filter: 'trashed' }),
+            icon: TrashIcon,
+            current: route().current('forms.entries.index', { form: props.form, filter: 'trashed' }),
+        },
+    ];
+});
+const currentTab = computed(() => {
+    return tabs.value.find(tab => tab.current);
+})
 
 const selectEntry = (entry) => {
     if (entry.uuid === selectedEntry.value?.uuid) {
@@ -229,22 +237,14 @@ const deselectEntry = () => {
                     <!-- No entry selected -->
                     <div v-else class="hidden md:flex md:flex-col md:justify-center md:h-full md:px-6 md:py-5">
                         <div class="text-center">
-                            <svg
-                                class="mx-auto h-12 w-12 text-gray-400"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1"
-                                stroke="currentColor"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3"
-                                />
-                            </svg>
-                            <h3 class="mt-2 text-base font-semibold text-gray-900">Inbox</h3>
+                            <div class="inline-block mx-auto text-gray-400">
+                                <ThinArrowDownIcon v-if="currentTab.name === 'Inbox'" />
+                                <ThinArchiveBoxIcon v-if="currentTab.name === 'Archive'" />
+                                <ThinTrashIcon v-if="currentTab.name === 'Trash'" />
+                            </div>
+                            <h3 class="mt-2 text-base font-semibold text-gray-900">
+                                {{ tabs.find(tab => tab.current)?.name }}
+                            </h3>
                             <p class="mt-1 text-sm text-gray-500">
                                 You have {{ displayNumber(entries.total, 999_999) }} entries here.
                             </p>
