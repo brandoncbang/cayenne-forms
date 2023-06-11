3befe6be3a444fe8b397eee7a4277f47c4d802b6
Brandon Bang <brandoncharlesbang@gmail.com>
2023-06-11 17:39:20 -0400

    Center delete button text

--- resources/js/Pages/Entries/Partials/Entry.vue
+++ resources/js/Pages/Entries/Partials/Entry.vue
@@ -99,7 +99,7 @@ const destroy = () => {
 
                 <button
                     v-if="entryIsTrashed"
-                    class="inline-flex items-center rounded-md bg-rose-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
+                    class="inline-flex items-center justify-center rounded-md bg-rose-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-600"
                     type="button"
                     @click="destroy"
                 >
