6404c0275aaf3f48db510b8505a38593a4896df8
Brandon Bang <brandoncharlesbang@gmail.com>
2023-06-13 13:32:29 -0400

    Re-add `/api` prefix to default get user API route

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('/f/{form:uuid}', [EntryController::class, 'store'])->name('forms.entries.store');
 Route::get('/success', fn () => view('entries.success'))->name('entries.success');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::prefix('api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+})->middleware('auth:sanctum');
