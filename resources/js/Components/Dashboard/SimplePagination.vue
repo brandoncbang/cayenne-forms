b36bec85658e250f446c037b470f6d71629abfae
Brandon Bang <brandoncharlesbang@gmail.com>
2023-06-14 16:26:33 -0400

    Add link titles

--- resources/js/Components/Dashboard/SimplePagination.vue
+++ resources/js/Components/Dashboard/SimplePagination.vue
@@ -32,6 +32,7 @@ defineProps({
                 :href="paginator.prev_page_url"
                 class="relative inline-flex items-center rounded-l-md px-3 py-3 ring-1 ring-inset ring-gray-300 sm:px-2 sm:py-2 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
                 :class="{ 'text-gray-900': paginator.prev_page_url, 'text-gray-400': !paginator.prev_page_url }"
+                title="Previous page"
             >
                 <span class="sr-only">Previous</span>
                 <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
@@ -40,6 +41,7 @@ defineProps({
                 :href="paginator.next_page_url"
                 class="relative inline-flex items-center rounded-r-md px-3 py-3 ring-1 ring-inset ring-gray-300 sm:px-2 sm:py-2 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
                 :class="{ 'text-gray-900': paginator.next_page_url, 'text-gray-400': !paginator.next_page_url }"
+                title="Next page"
             >
                 <span class="sr-only">Next</span>
                 <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
