ac1b131aa0ff28cdfd80a17626ee007cb54a5fd4
Brandon Bang <brandoncharlesbang@gmail.com>
2023-06-14 16:26:20 -0400

    Refresh state when navigating pagination links

--- resources/js/Components/Dashboard/SimplePagination.vue
+++ resources/js/Components/Dashboard/SimplePagination.vue
@@ -32,7 +32,6 @@ defineProps({
                 :href="paginator.prev_page_url"
                 class="relative inline-flex items-center rounded-l-md px-3 py-3 ring-1 ring-inset ring-gray-300 sm:px-2 sm:py-2 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
                 :class="{ 'text-gray-900': paginator.prev_page_url, 'text-gray-400': !paginator.prev_page_url }"
-                preserve-state
             >
                 <span class="sr-only">Previous</span>
                 <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
@@ -41,7 +40,6 @@ defineProps({
                 :href="paginator.next_page_url"
                 class="relative inline-flex items-center rounded-r-md px-3 py-3 ring-1 ring-inset ring-gray-300 sm:px-2 sm:py-2 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
                 :class="{ 'text-gray-900': paginator.next_page_url, 'text-gray-400': !paginator.next_page_url }"
-                preserve-state
             >
                 <span class="sr-only">Next</span>
                 <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
