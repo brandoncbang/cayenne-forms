d014cd39c96bed46f091e4771b4da38e47a98f6c
Brandon Bang <brandoncharlesbang@gmail.com>
2023-07-12 13:30:40 -0400

    Remove unnecessary scrolling

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -147,8 +147,8 @@ const slots = useSlots();
                 </div>
             </main>
             <footer class="flex-shrink-0">
-                <div class="mx-auto max-w-3xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                    <div class="border-t border-gray-200 py-8 text-center text-sm text-gray-500">
+                <div class="mx-auto max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+                    <div class="text-center text-sm text-gray-500">
                         <p>
                             Powered by
                         </p>
