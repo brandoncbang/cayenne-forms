5ce9c8a8bdc0cf8c0476464aa8258520f1c5bbfe
Brandon Bang <brandoncharlesbang@gmail.com>
2023-07-12 15:02:26 -0400

    Update Forgot Password page layout

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -5,6 +5,8 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, useForm } from '@inertiajs/vue3';
+import { InformationCircleIcon } from '@heroicons/vue/20/solid/index.js';
+import TextField from '@/Components/Dashboard/TextField.vue';
 
 defineProps({
     status: {
@@ -22,39 +24,42 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Forgot Password" />
-
-        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset
-            link that will allow you to choose a new one.
-        </div>
-
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-            {{ status }}
-        </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
+    <GuestLayout title="Forgot password">
+        <div v-if="status" class="rounded-md bg-green-50 p-4 mb-10">
+            <div class="flex">
+                <div class="flex-shrink-0">
+                    <InformationCircleIcon class="h-5 w-5 text-green-400" aria-hidden="true" />
+                </div>
+                <div class="ml-3 flex-1">
+                    <p class="text-sm text-green-700">{{ status }}</p>
+                </div>
             </div>
+        </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
-                </PrimaryButton>
+        <form @submit.prevent="submit" class="space-y-6">
+            <p class="text-sm leading-6 text-gray-600">
+                Forgot your password? No problem. Just let us know your email address and we will email you a password reset
+                link that will allow you to choose a new one.
+            </p>
+
+            <TextField
+                id="email"
+                v-model="form.email"
+                type="email"
+                label="Email address"
+                :error="form.errors.email"
+                autocomplete="username"
+                autofocus
+                required
+            />
+
+            <div>
+                <button
+                    type="submit"
+                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                    Email password reset link
+                </button>
             </div>
         </form>
     </GuestLayout>
