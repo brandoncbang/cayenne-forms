2cfd1ed6bedd26f5a0389d4e17c64cefea1b3cc9
Brandon Bang <brandoncharlesbang@gmail.com>
2023-07-14 16:36:04 -0400

    Fix page overflow

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -16,7 +16,7 @@ const slots = useSlots();
 <template>
     <Head :title="title" />
 
-    <div class="flex flex-col items-stretch">
+    <div class="flex flex-col items-stretch min-w-0">
         <Disclosure as="nav" class="flex-shrink-0 bg-white shadow-sm" v-slot="{ open }">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div class="flex h-16 justify-between">
