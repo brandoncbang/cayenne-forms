4696e31cd3381ff3ced0ae98570633c5c32ee22d
Brandon Bang <brandoncharlesbang@gmail.com>
2023-07-16 12:23:04 -0400

    Add missing tests for unauthorized Form Deletion

--- tests/Feature/DeleteFormTest.php
+++ tests/Feature/DeleteFormTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature;
 
 use App\Models\Form;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
@@ -29,12 +31,28 @@ class DeleteFormTest extends TestCase
     #[Test]
     public function guest_cannot_delete_a_form()
     {
-        $this->markTestIncomplete();
+        $form = Form::factory()->create();
+
+        $response = $this->delete("/forms/{$form->uuid}");
+
+        $response->assertRedirect('/');
+
+        $this->assertDatabaseCount(Form::class, 1);
     }
 
     #[Test]
     public function user_cannot_delete_the_entry_of_another_user()
     {
-        $this->markTestIncomplete();
+        $form = Form::factory()->create();
+
+        $nonOwner = User::factory()->create();
+
+        $response = $this
+            ->actingAs($nonOwner)
+            ->delete("/forms/{$form->uuid}");
+
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+
+        $this->assertDatabaseCount(Form::class, 1);
     }
 }
