9eb9284a8a452b8154ec5cf7175950b0817f487d
Brandon Bang <brandoncharlesbang@gmail.com>
2023-07-17 16:11:19 -0400

    Use PHPUnit test attribute

--- tests/Feature/Auth/PasswordUpdateTest.php
+++ tests/Feature/Auth/PasswordUpdateTest.php
@@ -5,13 +5,15 @@ namespace Tests\Feature\Auth;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class PasswordUpdateTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_password_can_be_updated(): void
+    #[Test]
+    public function password_can_be_updated(): void
     {
         $user = User::factory()->create();
 
@@ -31,7 +33,8 @@ class PasswordUpdateTest extends TestCase
         $this->assertTrue(Hash::check('new-password', $user->refresh()->password));
     }
 
-    public function test_correct_password_must_be_provided_to_update_password(): void
+    #[Test]
+    public function correct_password_must_be_provided_to_update_password(): void
     {
         $user = User::factory()->create();
 
--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -4,13 +4,15 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
 class ProfileTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_profile_page_is_displayed(): void
+    #[Test]
+    public function profile_page_is_displayed(): void
     {
         $user = User::factory()->create();
 
@@ -21,7 +23,8 @@ class ProfileTest extends TestCase
         $response->assertOk();
     }
 
-    public function test_profile_information_can_be_updated(): void
+    #[Test]
+    public function profile_information_can_be_updated(): void
     {
         $user = User::factory()->create();
 
@@ -43,7 +46,8 @@ class ProfileTest extends TestCase
         $this->assertNull($user->email_verified_at);
     }
 
-    public function test_email_verification_status_is_unchanged_when_the_email_address_is_unchanged(): void
+    #[Test]
+    public function email_verification_status_is_unchanged_when_the_email_address_is_unchanged(): void
     {
         $user = User::factory()->create();
 
@@ -61,7 +65,8 @@ class ProfileTest extends TestCase
         $this->assertNotNull($user->refresh()->email_verified_at);
     }
 
-    public function test_user_can_delete_their_account(): void
+    #[Test]
+    public function user_can_delete_their_account(): void
     {
         $user = User::factory()->create();
 
@@ -79,7 +84,8 @@ class ProfileTest extends TestCase
         $this->assertNull($user->fresh());
     }
 
-    public function test_correct_password_must_be_provided_to_delete_account(): void
+    #[Test]
+    public function correct_password_must_be_provided_to_delete_account(): void
     {
         $user = User::factory()->create();
 
