e0ed742a24258d9d1b27466849771801e2423ae6
Brandon Bang <brandoncharlesbang@gmail.com>
2023-07-17 12:04:25 -0400

    Handle null Markdown content

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -5,8 +5,12 @@ use Illuminate\Support\Str;
 /**
  * Escape a string, so it can be displayed inside Markdown content, such as a Markdown email template.
  */
-function escapeMarkdown(string $markdown): string
+function escapeMarkdown(?string $markdown): string
 {
+    if (is_null($markdown)) {
+        return '';
+    }
+
     $searches = [
         '\\', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';',
         '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~',
